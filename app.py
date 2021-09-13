157b2e5af6ac4673d5d07914b0f806595f0f9d9d
leubenia <hdodo777@gmail.com>
2021-09-13 22:50:05 +0900

    기본 틀만짜보자

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/header')
+def header():
+    return render_template('header.html')
+    
+@app.route('/footer')
+def footer():
+    return render_template('footer.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
